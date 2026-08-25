af0798d7a0da13bb3452bfa330d83c26b47cb584
abdulraheem michael <michaeljaywhite2@gmail.com>
2026-08-25 01:26:01 +0100

    update

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+const copyButton = document.getElementById("copy-btn");
+const accountNumber = document.getElementById("account-number");
+
+copyButton.addEventListener("click", function() {
+  navigator.clipboard.writeText(accountNumber.textContent);
+
+  copyButton.textContent = "Copied";
+
+  setTimeout (function (){
+    copyButton.textContent = "copy"
+  }, 2000);
+});
+
+const progressBars = document.querySelectorAll(".progress-fill");
+progressBars.forEach(function(bar) {
+  const raised = Number(bar.dataset.raised);
+  const goal = Number(bar.dataset.goal);
+  const percentage = (raised / goal) * 100;
+  bar.style.width = percentage + "%";
+});
